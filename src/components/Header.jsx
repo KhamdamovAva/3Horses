2f8f8e29c56c2aa504d0c6a23d759fb1fefe33bc
JamshidPts <jamshidpts14@gmail.com>
2025-05-21 21:00:11 +0500

    i updated adaptive to mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,8 @@ function Header() {
                     backgroundImage: `url(${backImage})`,
                     backgroundBlendMode: 'overlay',
                 }}>
-                <h1 className='text-[64px] font-bold mb-4 leading-19 text-[#7c0000]'>Три коня - сохраняя <br />традиции</h1>
-                <a href='' className='bg-[#8c0000] text-white text-[15px] px-[40px] py-[15px] font-bold cursor-pointer transition-all duration-300 hover:bg-[#a00000]'>
+                <h1 className='text-[30px] sm:text-[45px] md:text-[64px] font-bold mb-4 md:leading-19 text-[#7c0000]'>Три коня - сохраняя <br />традиции</h1>
+                <a href='' className='bg-[#8c0000] text-white text-[12px] sm:text-[14px] md:text-[15px] px-[20px] sm:px-[30px] md:px-[40px] py-[11px] sm:py-[14px] md:py-[15px] font-bold cursor-pointer transition-all duration-300 hover:bg-[#a00000]'>
                     Подробнее о мероприятии
                 </a>
             </div>
