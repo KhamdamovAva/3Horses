3bd36eb10445ff7821b14c7b8fcbb1161736776a
KhamdamovAva <Ava.lifeone@gmail.com>
2025-05-21 21:42:54 +0500

    fixed responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,16 +4,16 @@ import footerLogo from '../assets/icons/footerLogo.svg'
 function Footer() {
   return (
     <div className="min-h-[204px] bg-[#262626] py-[20px]">
-      <div className='container'>
+      <div className='max-w-[1280px] mx-auto px-4'>
         <nav className="flex justify-center text-white pt-[40px] pb-[10px]">
-          <ul className="flex gap-[40px] text-[16px]">
+          <ul className="flex mb-[20px] sm:mb-0 gap-[10px] sm:gap-[30px] text-[14px] sm:text-[16px]">
             <li><a href="#">Главная</a></li>
             <li><a href="#">Мероприятия</a></li>
             <li><a href="#">Личный кабинет</a></li>
           </ul>
         </nav>
-        <div>
-          <img src={footerLogo} alt={footerLogo} />
+        <div >
+          <img className='mb-[20px] m-auto sm:m-0' src={footerLogo} alt={footerLogo} />
         </div>
         <div className='text-white text-center'>
           “Три коня”, 2025 год
