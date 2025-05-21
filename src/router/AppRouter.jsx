1b55be57d5bdefe3df0e07d132667d7123227237
KhamdamovAva <Ava.lifeone@gmail.com>
2025-05-21 15:54:15 +0500

    connected Registration

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
+import Registration from '../components/Registration'
 
 function AppRouter() {
     return (
         <>
             <Routes>
-                <Route path='/' element={<HomePage />}/>
+                <Route path='/' element={<HomePage />} />
+                <Route path='registration' element={<Registration />} />
             </Routes>
         </>
     )
