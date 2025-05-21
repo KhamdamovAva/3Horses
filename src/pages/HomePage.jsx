1064829770db3fb0ee74bff16c7be1dd4d41d470
KhamdamovAva <Ava.lifeone@gmail.com>
2025-05-21 14:50:32 +0500

    connected History.jsx to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import About from '../components/About'
+import History from '../components/History'
 
 function HomePage() {
   return (
     <>
-    <header>
-      <Navbar />
-      <Header />
-    </header>
-    <main>
+      <header>
+        <Navbar />
+        <Header />
+      </header>
+      <main>
         <About />
-    </main>
+        <History />
+      </main>
     </>
   )
 }
