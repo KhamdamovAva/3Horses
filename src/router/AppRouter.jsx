9d50f71af30fc55265f63f52b0464dc3de62650a
JamshidPts <jamshidpts14@gmail.com>
2025-05-21 13:06:13 +0500

    created AppRouter

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from '../pages/HomePage'
+
+function AppRouter() {
+    return (
+        <>
+            <Routes>
+                <Route path='/' element={<HomePage />}/>
+            </Routes>
+        </>
+    )
+}
+
+export default AppRouter
